642f5994a500e9b660faaef821121eb74842bd9f
prashant1012 <63799413+prashant1012@users.noreply.github.com>
2022-04-08 17:44:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,21 @@
   <?php
   include 'navbar.php';
   ?>
+    <?php
+
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "Indian_Bank";
+
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
+© 2021 GitHub, Inc.
   
       <div class="container-fluid">
       <!-- Introduction section -->
